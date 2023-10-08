60b49d360311b98b5706150b23eaa12c18e30716
Brian Takita <brian.takita@gmail.com>
2023-10-08 02:19:15 -0400

    + @proseui/-dev-bin
    	+ tmux-dev
    	+ tmux-rename-window
    
    + @proseui/.github
    
    + proseui.net
    
    + .envrc

---
+++ app/-dev-bin/bin/tmux-dev.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+DEV_DIR_DEFAULT=~/work/proseui/-dev
+DEV_DIR="${DEV_DIR:-$DEV_DIR_DEFAULT}"
+
+cd $DEV_DIR
+tmux-rename-window
+tmux split-window -h $SHELL
+tmux split-window -v $SHELL
+tmux send-keys 'tig' 'C-m'
+tmux select-pane -t 0
+tmux split-window -v $SHELL
+tmux select-pane -t 0
+
+tmux select-layout tiled
+tmux move-window -t 0
+tmux select-pane -t 1
+tmux select-window -t 0
